b93ff9969e5d7a720e2f16e959187be7370f11a8
pnevmoslon1 <rinatv305@gmail.com>
2024-04-26 05:10:01 +0400

    master

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,5 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        array_2D a = new array_2D(3, 4);
-        array_array c = new array_array(4);
 
-        a.arrayPrint();
-        a.func();
-        a.arrayPrint();
-
-
-
-    }
 }
