df78e6c7d7d76cde81a7c4330f62fa3481bf842a
pnevmoslon1 <rinatv305@gmail.com>
2024-04-26 05:42:14 +0400

    lab4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
+import java.io.*;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        String s="";
+        try(FileReader reader = new FileReader("input.txt"))
+        { // читаем посимвольно
+            int c;
+            while((c=reader.read())!=-1){
+                s+=(char)c; }
+        }
+ catch(IOException ex){
+            System.out.println(ex.getMessage());
+        }
+        String out = "";
+        char c;
+        for (int i = 0; i < s.length(); i++) {
+            c = s.charAt(i);
 
+            out += (char)(c - 1);
+        }
+
+
+
+        try (FileWriter writer = new FileWriter("output.txt",
+                false))
+        { // подготавливаем строку для записи
+            writer.write(out);
+
+            // сохранение записи
+            writer.flush();
+        }
+ catch(IOException ex){
+            System.out.println(ex.getMessage());
+        }
+
+    }
 }
